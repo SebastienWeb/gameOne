302ec1991ead4f72f7563c68503087b42e0d1f99
SebastienWeb <sebastienweb27@gmail.com>
2023-02-06 14:16:47 +0100

    Project finish

--- main.js
+++
@@ -1,8 +0,0 @@
-const form = document.getElementById("form");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const firstname = document.getElementById("first").value;
-  const display = document.getElementById("display");
-  display.innerText = firstname;
-});
--- modal.js
+++ modal.js
@@ -25,3 +25,113 @@ function launchModal() {
 function closeNav() {
   modalbg.style.display = "none";
 }
+
+const modalSucces = document.getElementById("modalSucces");
+const close = document.getElementById("close");
+close.addEventListener("click", closeNavSucces);
+function closeNavSucces() {
+  modalSucces.style.display = "none";
+}
+function modalValidation() {
+  const modalSucces = document.getElementById("modalSucces");
+  modalSucces.style.display = "block";
+}
+
+function error(message) {
+  return message;
+}
+function validedFirstName() {
+  const firstName = document.getElementById("first");
+  const errorFirst = document.getElementById("errorFirst");
+  if (firstName.value === "") {
+    errorFirst.innerText = error("Champs Obligatoire");
+    return false;
+  } else if (firstName.value.trim().length < 2) {
+    errorFirst.innerText = error("Veuillez entrer 2 caractères ou plus");
+    return false;
+  } else {
+    return true;
+  }
+}
+function validatedLast() {
+  const lastName = document.getElementById("last");
+  const errorLast = document.getElementById("errorLast");
+  if (lastName.value === "") {
+    errorLast.innerText = error("Champs Obligatoire");
+    return false;
+  } else if (lastName.value.trim().length < 2) {
+    errorLast.innerText = error("Veuillez entrer 2 caractères ou plus");
+    return false;
+  } else {
+    return true;
+  }
+}
+function validatedBirthDay() {
+  const birthDay = document.getElementById("birthdate");
+  const errorBirth = document.getElementById("errorBirth");
+  if (birthDay.value === "") {
+    errorBirth.innerText = error("Vous devez entrer votre date de naissance.");
+    return false;
+  } else {
+    return true;
+  }
+}
+function validatedCheckBoxCondition() {
+  const checkbox1 = document.getElementById("checkbox1");
+  const errorCheckbox = document.getElementById("errorCheckbox");
+  if (checkbox1.checked === false) {
+    errorCheckbox.innerText = error(
+      "Vous devez vérifier que vous acceptez les termes et conditions."
+    );
+    return false;
+  } else {
+    return true;
+  }
+}
+function valitedLocations() {
+  const locations = document.querySelectorAll("#locations .checkbox-input");
+  const errorLocation = document.getElementById("errorLocation");
+  for (let i = 0; i < locations.length; i++) {
+    if (locations[i].checked) {
+      return true;
+    }
+  }
+  errorLocation.innerText = error("Vous devez choisir une option");
+  return false;
+}
+function valitedEmail() {
+  const email = document.getElementById("email");
+  const regexEmail = /.+\@.+\..+/;
+  const errorEmail = document.getElementById("errorEmail");
+  if (email.value.trim().match(regexEmail)) {
+    return true;
+  }
+  errorEmail.innerText = error("Email non valide");
+  return false;
+}
+function valitedTournament() {
+  const quantity = document.getElementById("quantity");
+  const errorQuantity = document.getElementById("errorQuantity");
+  if (
+    quantity.value.trim().length === 0 ||
+    quantity.value.trim() < 0 ||
+    quantity.value.trim() > 99
+  ) {
+    errorQuantity.innerText = error("Veuillez saisir une quantité valide");
+    return false;
+  }
+  return true;
+}
+
+// function validate
+function validate() {
+  return (
+    validedFirstName() &&
+    validatedLast() &&
+    valitedEmail() &&
+    validatedBirthDay() &&
+    valitedTournament() &&
+    valitedLocations() &&
+    validatedCheckBoxCondition()
+  );
+}
